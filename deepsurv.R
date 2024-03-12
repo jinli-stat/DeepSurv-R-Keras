1e0119e054afa1d1d90bce321bf71868c17c1214
jinli-stat <jinli.main@outlook.com>
2024-03-12 22:14:13 +0800

    upup

--- deepsurv.R
+++ deepsurv.R
@@ -3,26 +3,27 @@ library(keras)
 library(survival)
 # BiocManager::install("survcomp")
 
-negative_log_likelihood <- function(E) {
-  function(y_true, y_pred) {
-    hazard_ratio <- k_exp(y_pred)
-    log_risk <- k_log(k_cumsum(hazard_ratio))
-    uncensored_likelihood <- k_transpose(y_pred) - log_risk
-    censored_likelihood <- uncensored_likelihood * E
-    neg_likelihood_ <- -k_sum(censored_likelihood)
-    
-    # num_observed_events <- k_cumsum(E)
-    # num_observed_events <- k_cast(num_observed_events, dtype = "float32")
-    
-    num_observed_events <- k_constant(1, dtype = "float32")
-    
-    neg_likelihood <- neg_likelihood_ / num_observed_events
-    
-    return(neg_likelihood)
-  }
+neg_log_likelihd <- function(y_true, y_pred) {
+  event <- y_true[, 1]
+  time <- y_true[, 2]
+  mask <- k_cast(time <= k_reshape(time, shape = c(-1, 1)), dtype = "float32")
+  
+  log_loss <- k_log(k_sum(mask * k_exp(y_pred), axis = 1))
+  neg_log_loss <- -k_sum(event * (y_pred - log_loss))
+  
+  return(neg_log_loss / k_sum(event))
 }
 
-build_deepsurv <- function(num_input, num_layer, num_nodes, string_activation, num_l2, num_dropout, num_lr, lr_decay) {
+
+build_deepsurv <- function(num_input, 
+                           num_layer, 
+                           num_nodes, 
+                           string_activation, 
+                           num_l2, 
+                           num_dropout, 
+                           num_lr, 
+                           lr_decay) {
+  
   input_layer <- layer_input(shape = c(num_input),
                              dtype = "float32")
   
@@ -30,54 +31,81 @@ build_deepsurv <- function(num_input, num_layer, num_nodes, string_activation, n
   
   for (i in 1:num_layer) {
     hidden_layers <- hidden_layers %>%
-      layer_dense(units = num_nodes, activation = string_activation, kernel_regularizer = regularizer_l2(num_l2)) %>%
+      layer_dense(units = num_nodes, 
+                  activation = string_activation, 
+                  # kernel_regularizer = regularizer_l2(num_l2),
+                  kernel_initializer = "glorot_uniform") %>%
       layer_dropout(rate = num_dropout)
   }
   
   hidden_layers <- hidden_layers %>%
-    layer_dense(units = 1, activation = NULL)
+    layer_dense(units = 1, 
+                activation = "linear", 
+                kernel_regularizer = regularizer_l2(num_l2)) %>% 
+    layer_activity_regularization(l2 = num_l2)
   
   model <- keras_model(input_layer, hidden_layers)
   
   model %>% compile(
-    optimizer = optimizer_nadam(learning_rate = num_lr, decay = lr_decay, clipnorm=1.), 
-    loss = negative_log_likelihood(E_train),
-    metrics = NULL
+    loss = neg_log_likelihd,
+    optimizer = optimizer_nadam(learning_rate = num_lr, 
+                                decay = lr_decay, 
+                                clipnorm=1.)
   )
+}
 
+c_index <- function(LP_pred, Y, E){
+  c_index <- survcomp::concordance.index(LP_pred, Y, E)$c.index
+  return(c_index)
 }
 
 
-model = build_deepsurv(num_input=6,
-                  num_layer= 3,
-                  string_activation ="relu",
-                  num_nodes = 48,
-                  num_lr = 0.067,
-                  num_l2 = 16.094,
-                  num_dropout = 0.147,
-                  lr_decay = 6.494e-4)
+# BiocManager::install("rhdf5")
+X_train <- rhdf5::h5read("Data/simulated_linear.h5", "train/x") %>% t()
+event_train <- rhdf5::h5read("Data/simulated_linear.h5", "train/e") %>% c()
+time_train <- rhdf5::h5read("Data/simulated_linear.h5", "train/t") %>% c()
+Y_train <- cbind(event_train, time_train)
 
-es <- callback_early_stopping(monitor = "loss", patience = 5, verbose = 1)
+X_test <- rhdf5::h5read("Data/simulated_linear.h5", "test/x") %>% t()
+event_test <- rhdf5::h5read("Data/simulated_linear.h5", "test/e") %>% c()
+time_test <- rhdf5::h5read("Data/simulated_linear.h5", "test/t") %>% c()
+Y_test <- cbind(event_test, time_test)
+
+m1 <- colMeans(X_train)
+s1 <- apply(X_train, 2, sd)
+X_train <- scale(X_train, center = m1, scale = s1)
+X_test <- scale(X_test, center = m1, scale = s1)
+
+n_patients_train <- dim(X_train)[1]
+n_features <- dim(X_train)[2]
+
+model = build_deepsurv(num_input=n_features,
+                  num_layer= 1,
+                  string_activation ="selu",
+                  num_nodes = 4,
+                  num_lr = 2.922e-4,
+                  num_l2 = 1.999,
+                  num_dropout = 0.375,
+                  lr_decay = 3.579e-4)
+
+es <- callback_early_stopping(monitor = "loss", patience = 20, verbose = 1)
 
 # Add the callback to a list
 callbacks_list <- list(es)
 
-epochs <- 30
+epochs <- 500
 history <- model %>% fit(X_train, Y_train,
-                         batch_size = n_patients_train,
+                         batch_size = 1000,
                          epochs = epochs,
-                         shuffle = FALSE,
+                         shuffle = TRUE,
                          callbacks = callbacks_list
 )
+
 
 LP_train <- model %>% predict(X_train)
 LP_test <- model %>% predict(X_test)
 
 
-c_index <- function(X, Y, E){
-  LP_pred <- model %>% predict(X)
-  c_ind <- survcomp::concordance.index(LP_pred, Y, E)$c.index
-  return(c_ind)
-}
-c_index(X_train, Y_train, E_train)
-c_index(X_test, Y_test, E_test)
+print(c_index(LP_train, Y_train[, 2], Y_train[, 1]))
+
+print(c_index(LP_test, Y_test[, 2], Y_test[, 1]))
--- simple.R
+++ simple.R
@@ -7,13 +7,14 @@ library(ggplot2)
 
 # BiocManager::install("rhdf5")
 X_train <- rhdf5::h5read("Data/whas.h5", "train/x") %>% t()
-E_train <- rhdf5::h5read("Data/whas.h5", "train/e") %>% c()
-Y_train <- rhdf5::h5read("Data/whas.h5", "train/t") %>% c()
+event_train <- rhdf5::h5read("Data/whas.h5", "train/e") %>% c()
+time_train <- rhdf5::h5read("Data/whas.h5", "train/t") %>% c()
+Y_train <- cbind(event_train, time_train)
 
 X_test <- rhdf5::h5read("Data/whas.h5", "test/x") %>% t()
-E_test <- rhdf5::h5read("Data/whas.h5", "test/e") %>% c()
-Y_test <- rhdf5::h5read("Data/whas.h5", "test/t") %>% c()
-
+event_test <- rhdf5::h5read("Data/whas.h5", "test/e") %>% c()
+time_test <- rhdf5::h5read("Data/whas.h5", "test/t") %>% c()
+Y_test <- cbind(event_test, time_test)
 
 n_patients_train <- dim(X_train)[1]
 n_features <- dim(X_train)[2]
@@ -23,28 +24,14 @@ s1 <- apply(X_train, 2, sd)
 X_train <- scale(X_train, center = m1, scale = s1)
 X_test <- scale(X_test, center = m1, scale = s1)
 
-sort_idx <- order(Y_train, decreasing = TRUE)
-X_train <- X_train[sort_idx, ]
-Y_train <- Y_train[sort_idx]
-E_train <- E_train[sort_idx]
+neg_log_likelihd <- function(y_true, y_pred) {
+  event <- y_true[, 1]
+  time <- y_true[, 2]
+  mask <- k_cast(time <= k_reshape(time, shape = c(-1, 1)), dtype = "float32")
 
-negative_log_likelihood <- function(E) {
-  function(y_true, y_pred) {
-    hazard_ratio <- k_exp(y_pred)
-    log_risk <- k_log(k_cumsum(hazard_ratio))
-    uncensored_likelihood <- k_transpose(y_pred) - log_risk
-    censored_likelihood <- uncensored_likelihood * E
-    neg_likelihood_ <- -k_sum(censored_likelihood)
-    
-    # num_observed_events <- k_cumsum(E)
-    # num_observed_events <- k_cast(num_observed_events, dtype = "float32")
-
-    num_observed_events <- k_constant(1, dtype = "float32")
-
-    neg_likelihood <- neg_likelihood_ / num_observed_events
-
-    return(neg_likelihood)
-  }
+  log_loss <- k_log(k_sum(mask * k_exp(y_pred), axis = 1))
+  neg_log_loss <- -k_sum(event * (y_pred - log_loss))
+  return(neg_log_loss / k_sum(event))
 }
 
 activation <- "relu"
@@ -65,11 +52,11 @@ model %>%
   layer_activity_regularization(l2 = l2_reg)
 
 # optimizer_nadam
-optimizer <- optimizer_nadam(learning_rate = learning_rate, decay = lr_decay, clipnorm=1.)
+optimizer <- optimizer_nadam(learning_rate = learning_rate, decay = lr_decay, clipnorm = 1.)
 
 # Compile the model and show a summary of it
-model %>% compile(loss = negative_log_likelihood(E_train), optimizer = optimizer)
-
+model %>% compile(loss = neg_log_likelihd, optimizer = optimizer)
+# negative_log_likelihood(E_train)
 
 es <- callback_early_stopping(monitor = "loss", patience = 20, verbose = 1)
 
@@ -78,30 +65,27 @@ callbacks_list <- list(es)
 
 epochs <- 500
 history <- model %>% fit(X_train, Y_train,
-  batch_size = n_patients_train,
+  batch_size = 1000,
   epochs = epochs,
-  shuffle = FALSE,
+  shuffle = TRUE,
   callbacks = callbacks_list
 )
+
+LP_train <- model %>% predict(X_train)
+LP_test <- model %>% predict(X_test)
+
+# BiocManager::install("survcomp")
+c_index_train <- survcomp::concordance.index(LP_train, Y_train[, 2], Y_train[, 1])$c.index
+cat(paste0("c-index of training dataset = ", c_index_train, "\n"))
+
+c_index_test <- survcomp::concordance.index(model %>% predict(X_test), Y_test[, 2], Y_test[, 1])$c.index
+cat(paste0("c-index of testing dataset = ", c_index_test, "\n"))
 
 # Y_pred_train <- exp(-(model %>% predict(X_train)))
 # lifelines <- import("lifelines")
 # c_index_train <- lifelines$utils$concordance_index(Y_train, Y_pred_train, E_train)
 # cat(paste0("c-index of training dataset = ", c_index_train, "\n"))
 
-
-LP_train <- model %>% predict(X_train)
-LP_test <- model %>% predict(X_test)
-
-# BiocManager::install("survcomp")
-c_index_train <- survcomp::concordance.index(LP_train, Y_train, E_train)$c.index
-cat(paste0("c-index of training dataset = ", c_index_train, "\n"))
-
-c_index_test <- survcomp::concordance.index(model %>% predict(X_test), Y_test, E_test)$c.index
-cat(paste0("c-index of testing dataset = ", c_index_test, "\n"))
-
-
 # install.packages("mlr3proba", repos = "https://mlr-org.r-universe.dev")
-library(mlr3proba)
-blhz <- breslow(Y_train, E_train, LP_train, LP_test[3], eval_times=2, type = "cumhaz")
-
+# library(mlr3proba)
+# blhz <- breslow(Y_train, E_train, LP_train, LP_test[3], eval_times = 2, type = "cumhaz")
